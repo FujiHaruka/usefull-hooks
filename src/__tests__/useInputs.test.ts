ac0f00c853a571a86738fadfe0968b92d6763d91
FujiHaruka <fujiharuka@realglobe.jp>
2019-04-18 14:58:04 +0900

    Add test

--- src/__tests__/useInputs.test.ts
+++ src/__tests__/useInputs.test.ts
@@ -6,11 +6,13 @@ afterEach(cleanup)
 
 describe('useInputs', () => {
   const render = () =>
-    renderHook(() =>
-      useInputs({
-        name: 'x',
-        age: 20,
-      }),
+    renderHook((arg: any) =>
+      useInputs(
+        arg || {
+          name: 'x',
+          age: 20,
+        },
+      ),
     )
   const event = (value: any) => ({
     target: {
@@ -34,5 +36,16 @@ describe('useInputs', () => {
     act(() => result.current.reset())
     expect(result.current.inputs.name).toBe('x')
     expect(result.current.inputs.age).toBe(20)
+  })
+
+  it('should reset other values', () => {
+    const { rerender, result } = render()
+    rerender({
+      name: 'x',
+      age: 10,
+    })
+
+    act(() => result.current.reset())
+    expect(result.current.inputs.age).toBe(10)
   })
 })
